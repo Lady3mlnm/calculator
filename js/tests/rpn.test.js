e510faa1ab830a9221fe311f319c6a9dea2bb2f3
Lady3mlnm <lady3mlnm@gmail.com>
2017-07-25 22:47:39 +0300

    Исправление бага
    Исправление бага, когда в конструкции встречается фрагмент '(-('. Улучшены тестовые функции.

--- js/tests/rpn.test.js
+++ js/tests/rpn.test.js
@@ -2,10 +2,12 @@
  * Created by Nancy Vasilyeva <gothness@ymail.com>
  * Date: 12.07.2017
  */
-
-console.log(calculate("1"));
-console.log(calculate(calculateFormat("(1)")));
-console.log(calculate(calculateFormat("1+3")));
-console.log(calculate(calculateFormat("((-6.6+(10.4+(-4)))/(1+4^2)+1)")));
-console.log(calculate(calculateFormat("-1+(-6.6 +  (10.4   +(-4)))/(1+4^2)+1--1")));
-console.log(calculate(calculateFormat("-(1+1)")));
+console.group("Test block");
+console.log(calculate("1"),' vs ',1);
+console.log(calculate(calculateFormat("(1)")),' vs ',1);
+console.log(calculate(calculateFormat("1+3")),' vs ',4);
+console.log(calculate(calculateFormat("5 +((1 + 2) * 4)   - 3")),' vs ',14);
+console.log(calculate(calculateFormat("((-6.6+(10.4+(-4)))/(1+4^2)+1)")),' vs ',0.988235294117647);
+console.log(calculate(calculateFormat("-1+(-6.6 +  (10.4   +(-4)))/(1+4^2)+1--1")),' vs ',0.988235294117647);
+console.log(calculate(calculateFormat("-(1+1)+(-(2+2))+(-(3+3))")),' vs ',-12);
+console.groupEnd();
